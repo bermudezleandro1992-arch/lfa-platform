57e6b4daf0cb63bca34757d770f0df584c73cc6f
SomosLFA <somoslfa@lfaofficial.com>
2026-04-28 02:34:04 -0300

    fix: auth popup blank + CSP blocks fixed for somoslfa.com authDomain
    - next.config.mjs: exclude /__/** paths from security headers
      (CSP was blocking Firebase auth handler scripts → popup en blanco)
    - Add /__/firebase/:path* rewrite (Firebase SDK resources)
    - CSP script-src: add *.firebaseapp.com and *.firebase.com
    - CSP connect-src: add identitytoolkit, securetoken, *.firebaseapp.com
    - CSP frame-src: add somoslfa.com

--- next.config.mjs
+++ next.config.mjs
@@ -30,7 +30,7 @@ const securityHeaders = [
     value: [
       "default-src 'self'",
       // Scripts: propios + Firebase Auth + Google login + reCAPTCHA Enterprise + Facebook + GTM
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://www.google.com https://www.gstatic.com https://apis.google.com https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://connect.facebook.net https://www.googletagmanager.com",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://www.google.com https://www.gstatic.com https://apis.google.com https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://connect.facebook.net https://www.googletagmanager.com https://*.firebaseapp.com https://*.firebase.com",
       // Estilos: propios + Google Fonts
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       // Fuentes: Google Fonts
@@ -38,9 +38,9 @@ const securityHeaders = [
       // Imágenes: propios + blobs + Google/Facebook avatars + Firebase Storage + banderas de idiomas
       "img-src 'self' blob: data: https://*.googleusercontent.com https://*.facebook.com https://*.fbcdn.net https://firebasestorage.googleapis.com https://www.gstatic.com https://flagcdn.com",
       // Conexiones: Firebase + reCAPTCHA Enterprise + Binance + Google + Twitch/YouTube + Facebook Graph + servidores juegos
-      "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com https://firebasestorage.googleapis.com https://*.cloudfunctions.net https://*.run.app https://www.google.com/recaptcha/ https://recaptchaenterprise.googleapis.com https://*.binance.com https://bpay.binanceapi.com https://accounts.google.com https://apis.google.com https://graph.facebook.com https://www.google-analytics.com https://www.googletagmanager.com https://api.twitch.tv https://*.twitch.tv wss://*.twitch.tv https://*.kick.com https://www.youtube.com https://utas.s2.ea.com https://utas.s3.ea.com https://utas.s4.ea.com https://utas.s5.ea.com https://utas.s6.ea.com https://utas.s7.ea.com https://utas.s8.ea.com https://utas.s9.ea.com https://utas.s10.ea.com https://utas.s11.ea.com https://we-pes-mobile.konami.net https://pes.konami.net https://pes-gameserver.konami.net",
+      "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com https://firebasestorage.googleapis.com https://*.cloudfunctions.net https://*.run.app https://www.google.com/recaptcha/ https://recaptchaenterprise.googleapis.com https://*.binance.com https://bpay.binanceapi.com https://accounts.google.com https://apis.google.com https://graph.facebook.com https://www.google-analytics.com https://www.googletagmanager.com https://api.twitch.tv https://*.twitch.tv wss://*.twitch.tv https://*.kick.com https://www.youtube.com https://utas.s2.ea.com https://utas.s3.ea.com https://utas.s4.ea.com https://utas.s5.ea.com https://utas.s6.ea.com https://utas.s7.ea.com https://utas.s8.ea.com https://utas.s9.ea.com https://utas.s10.ea.com https://utas.s11.ea.com https://we-pes-mobile.konami.net https://pes.konami.net https://pes-gameserver.konami.net https://*.firebaseapp.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com",
       // Frames: Google login + Firebase Auth + Facebook + reCAPTCHA + YouTube + Twitch + Kick (LFA TV)
-      "frame-src https://accounts.google.com https://www.google.com https://www.google.com/recaptcha/ https://*.firebaseapp.com https://www.facebook.com https://www.youtube.com https://www.youtube-nocookie.com https://player.twitch.tv https://clips.twitch.tv https://player.kick.com https://kick.com",
+      "frame-src https://accounts.google.com https://www.google.com https://www.google.com/recaptcha/ https://*.firebaseapp.com https://somoslfa.com https://www.facebook.com https://www.youtube.com https://www.youtube-nocookie.com https://player.twitch.tv https://clips.twitch.tv https://player.kick.com https://kick.com",
       // Workers: Service Worker
       "worker-src 'self' blob:",
     ].join('; '),
@@ -67,9 +67,9 @@ const nextConfig = {
 
   async headers() {
     return [
-      // Security headers en todas las rutas
+      // Security headers en todas las rutas EXCEPTO Firebase Auth handler paths
       {
-        source: '/(.*)',
+        source: '/((?!__/).*)',
         headers: securityHeaders,
       },
       // Cache agresivo para assets estáticos de Next.js (_next/static)
@@ -92,9 +92,15 @@ const nextConfig = {
 
   async rewrites() {
     return [
+      // Firebase Auth popup handler (necesario para authDomain custom)
       {
         source: '/__/auth/:path*',
         destination: 'https://lfaofficial.firebaseapp.com/__/auth/:path*',
+      },
+      // Firebase JS SDK resources
+      {
+        source: '/__/firebase/:path*',
+        destination: 'https://lfaofficial.firebaseapp.com/__/firebase/:path*',
       },
     ];
   },
